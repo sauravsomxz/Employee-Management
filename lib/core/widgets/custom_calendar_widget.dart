bb8224037db7b9431f0924478fe7550ff7aac6df
Sourav Maharana <saurav.maharana07@gmail.com>
2025-04-06 14:06:05 +0530

    Fixed height in calendar view for Web

--- lib/core/widgets/custom_calendar_widget.dart
+++ lib/core/widgets/custom_calendar_widget.dart
@@ -7,6 +7,7 @@ import 'package:employee_management/core/widgets/calendar/quick_button_selector.
 import 'package:employee_management/cubit/calendar/calendar_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:universal_platform/universal_platform.dart';
 
 class CustomCalendarDialog extends StatelessWidget {
   final Function(DateTime?) onDateSelected;
@@ -30,20 +31,25 @@ class CustomCalendarDialog extends StatelessWidget {
           final DateTime monthContext = selectedDate ?? DateTime.now();
 
           final screenHeight = constraints.maxHeight;
-          final isLargeScreen = constraints.maxWidth > 800;
+          final isLargeScreen = constraints.maxWidth > 500;
 
           // Set different heights depending on screen type
           final double dialogHeight =
               isLargeScreen
-                  ? screenHeight * 0.8
+                  ? screenHeight * 0.98
                   : calculateCalendarRows(monthContext) > 5
-                  ? screenHeight * 0.7
-                  : screenHeight * 0.63;
+                  ? screenHeight * 0.9
+                  : screenHeight * 0.9;
 
           return Center(
             child: Container(
               width: isLargeScreen ? 500 : double.infinity,
-              height: dialogHeight,
+              height:
+                  UniversalPlatform.isWeb
+                      ? dialogHeight
+                      : (calculateCalendarRows(monthContext) > 5
+                          ? MediaQuery.of(context).size.height * 0.7
+                          : MediaQuery.of(context).size.height * 0.63),
               padding: const EdgeInsets.all(16),
               decoration: BoxDecoration(
                 color: Colors.white,
